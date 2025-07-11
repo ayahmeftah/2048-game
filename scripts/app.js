cbb2eba052846bf0f59736527e1633a597012cc3
ayahmeftah <ayahmeftah123123@gmail.com>
2025-07-11 21:05:04 +0300

    adding random tiles functions is created and called before rendering

--- scripts/app.js
+++ scripts/app.js
@@ -1,33 +1,70 @@
-function init(){
+function init() {
 
     const gridElem = document.querySelector(".grid");
     const scoreElem = document.querySelector("#score-display");
     const gameOverElem = document.querySelector("#game-over");
+    const restartBtnElem = document.querySelector("#restart");
+
+
+    const gridWidth = 4;
+    const numberOfCells = gridWidth * gridWidth;
 
     let score = 0;
     let cells = [];
-    const gridWidth = 4;
-    const numberOfCells = gridWidth * gridWidth;
+    let grid = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ]
     let gameOver = false;
     let wonGame = false;
 
-    function render(){
+    function render() {
         createGrid();
+
     }
 
-    function createGrid(){
-        for (let i=0; i < numberOfCells; i++){
-            const cell = document.createElement('div');
-            cell.classList.add('cell');
-            cell.textContent = i;
-            cells.push(cell);
-            gridElem.appendChild(cell);
-            
+    function createGrid() {
+
+        gridElem.innerHTML = '';
+
+        for (let r = 0; r < gridWidth; r++) {
+            for (let c = 0; c < gridWidth; c++) {
+                const cell = document.createElement('div');
+                cell.classList.add('cell');
+                cell.textContent = grid[r][c] !== 0 ? grid[r][c] : '';
+                if (grid[r][c] > 0) {
+                    cell.classList.add(`t${grid[r][c]}`);
+                }
+                gridElem.appendChild(cell);
+            }
         }
-
     }
 
-    render()
+    function addRandomTile() {
+
+        let emptyCells = [];
+        for (let r = 0; r < gridWidth; r++) {
+            for (let c = 0; c < gridWidth; c++) {
+
+                if (grid[r][c] === 0) {
+                    emptyCells.push({ r, c });
+                }
+            }
+        }
+        if (emptyCells.length > 0) {
+            let { r, c } = emptyCells[Math.floor(Math.random() * emptyCells.length)];
+            grid[r][c] = Math.random() > 0.1 ? 2 : 4;
+        }
+    }
+
+
+    // restartBtnElem.addEventListener('click',init);
+
+    addRandomTile();
+    addRandomTile();
+    render();
 }
 
-document.addEventListener('DOMContentLoaded',init)
+document.addEventListener('DOMContentLoaded', init)
