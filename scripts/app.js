64b20ab480446d01c08f993a5a55cd9cc0d4bb29
ayahmeftah <ayahmeftah123123@gmail.com>
2025-07-14 22:24:58 +0300

    added best score storing functionality and new score messege

--- scripts/app.js
+++ scripts/app.js
@@ -11,7 +11,10 @@ function init() {
     const instructionsBtnElem = document.querySelector("#instructions");
     const instructionPopupElem = document.querySelector("#instruction-popup");
     const closeInstructBtnElem = document.querySelector(".close-btn");
+    const bestElem = document.querySelector("#best-display");
+    const newBestMsgElem = document.querySelector("#new-best-msg");
 
+    
     const gridWidth = 4;
     
     let score = 0;
@@ -26,18 +29,38 @@ function init() {
     ]
     let mergedTiles = [];
     
-
+    let best = localStorage.getItem("bestScore") || 0;
+    let hasShownBestMessage = false; 
+    
     function render() {
         popupElem.classList.add("hidden");
         popupElem.classList.remove("show");
         scoreElem.textContent = score
+        
+        bestElem.textContent = best
+        best = parseInt(best)
+
+        if (score > best) {
+            best = score;
+            localStorage.setItem("bestScore", best);
+            bestElem.textContent = best;
+
+            if (!hasShownBestMessage) {
+                newBestMsgElem.classList.remove("hidden");
+                hasShownBestMessage = true;
+
+                setTimeout(() => {
+                    newBestMsgElem.classList.add("hidden");
+                }, 2500);
+            }
+        }
         createGrid();
     }
 
     function createGrid() {
-        
+
         gridElem.innerHTML = '';
-        
+
         for (let r = 0; r < gridWidth; r++) {
             for (let c = 0; c < gridWidth; c++) {
                 const cell = document.createElement('div');
@@ -75,18 +98,18 @@ function init() {
             let randomCell = emptyCells[randomIndex];
             let r = randomCell.r;
             let c = randomCell.c;
-            
+
             grid[r][c] = Math.random() > 0.1 ? 2 : 4;
         }
-        
+
     }
 
     function handleKeyPress(event) {
-        
+
         if (gameOver || wonGame) return;
 
         const prevGrid = grid.map(row => [...row]);
-        
+
         /* This switch statement was taken from stackoverflow, here is the link of the question (answerd by Gibolt):
         https://stackoverflow.com/questions/5597060/detecting-arrow-key-presses-in-javascript
         The movement functions in each case were added by Ayah
@@ -98,13 +121,13 @@ function init() {
             case "ArrowRight":
                 moveRight()
                 break;
-                case "ArrowUp":
+            case "ArrowUp":
                 moveUp()
                 break;
-                case "ArrowDown":
-                    moveDown()
-                    break;
-                }
+            case "ArrowDown":
+                moveDown()
+                break;
+        }
 
         // Only add if grid changed (when tiles merge)
         if (!gridsEqual(prevGrid, grid)) {
@@ -123,7 +146,7 @@ function init() {
 
             let row = grid[r].filter((notEmptyCell) => notEmptyCell)
             for (let i = 0; i < row.length - 1; i++) {
-                
+
                 if (row[i] === row[i + 1]) {
                     row[i] *= 2
                     row[i + 1] = 0
@@ -145,9 +168,9 @@ function init() {
     function moveRight() {
         mergedTiles = [];
         for (let r = 0; r < gridWidth; r++) {
-            
+
             let row = grid[r].filter((notEmptyCell) => notEmptyCell)
-            
+
             for (let i = row.length - 1; i > 0; i--) {
                 if (row[i] === row[i - 1]) {
                     row[i] *= 2
@@ -164,16 +187,16 @@ function init() {
             while (row.length < gridWidth) {
                 row.unshift(0);
             }
-            
+
             grid[r] = row;
         }
     }
-    
+
     function moveUp() {
         mergedTiles = [];
         for (let c = 0; c < gridWidth; c++) {
             let col = []
-            
+
             for (let r = 0; r < gridWidth; r++) {
                 if (grid[r][c] !== 0) {
                     col.push(grid[r][c])
@@ -206,13 +229,13 @@ function init() {
         mergedTiles = [];
         for (let c = 0; c < gridWidth; c++) {
             let col = []
-            
+
             for (let r = 0; r < gridWidth; r++) {
                 if (grid[r][c] !== 0) {
                     col.push(grid[r][c])
                 }
             }
-            
+
             for (let i = col.length - 1; i > 0; i--) {
                 if (col[i] === col[i - 1]) {
 
@@ -260,7 +283,7 @@ function init() {
         for (let row of grid) {
             if (row.includes(0)) return false;
         }
-        
+
         // check for possible merges
         for (let r = 0; r < gridWidth; r++) {
             for (let c = 0; c < gridWidth; c++) {
@@ -286,14 +309,14 @@ function init() {
 
         popupMessageElem.textContent = message;
         popupScoreElem.textContent = "Score: " + score;
-        
+
         popupElem.classList.remove("hidden");
         popupElem.classList.add("show");
 
         overlayElem.classList.remove("hidden");
         overlayElem.classList.add("show");
     }
-    
+
     function gridsEqual(a, b) {
         for (let r = 0; r < gridWidth; r++) {
             for (let c = 0; c < gridWidth; c++) {
@@ -302,7 +325,7 @@ function init() {
         }
         return true;
     }
-    
+
     instructionsBtnElem.addEventListener("click", () => {
         instructionPopupElem.classList.remove("hidden");
         instructionPopupElem.classList.add("show");
